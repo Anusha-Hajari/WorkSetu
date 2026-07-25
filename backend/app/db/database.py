6f4fd6d9cf378eaa1a3ae0eb72136edbc983b6df
Anusha-Hajari <anuhajari311@gmail.com>
2026-07-25 15:51:46 +0530

    refactor: Move MongoDB credentials to environment variables
    -Remove hardcoded credentials from database.py
    - Use environment variables for MongoDB connection
    - Improve security by not storing passwords in code

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,9 +1,17 @@
+
+
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://Work_Setu_db_user:Work_Setu_AAA@ac-7all0jc-shard-00-00.wvqnx2w.mongodb.net:27017,ac-7all0jc-shard-00-01.wvqnx2w.mongodb.net:27017,ac-7all0jc-shard-00-02.wvqnx2w.mongodb.net:27017/work_setu_db?authSource=admin&replicaSet=atlas-ioftfv-shard-0&tls=true")
-db = client["work_setu_db"]
+load_dotenv()
 
-# UNIQUE EMAIL INDEX
+MONGO_URI = os.getenv("MONGO_URI")
+DB_NAME = os.getenv("DB_NAME")
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
 try:
     db.users.create_index("email", unique=True)
 except Exception as e:
